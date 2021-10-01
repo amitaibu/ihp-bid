c70e539da96c868f8f8fd5ce17d1c77b9ffbff53
Amitai Burstein <amitai@gizra.com>
2021-10-01 23:40:34 +0300

    docs

--- Config/Config.hs
+++ Config/Config.hs
@@ -6,6 +6,9 @@ import IHP.FrameworkConfig
 import Data.Fixed ( Pico )
 
 
+{-| If a Bid was sumibtted, the time in seconds
+    to wait before allowing to switch an Item from Active to Inactive.
+-}
 newtype BidPreventItemSwitchToInactiveRecentBidDiffSeconds = BidPreventItemSwitchToInactiveRecentBidDiffSeconds Data.Fixed.Pico
 
 config :: ConfigBuilder
