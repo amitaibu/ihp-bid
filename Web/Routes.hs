f721c3a7c8fb125938959391cfa20382a85a9f08
Amitai Burstein <amitai@gizra.com>
2021-10-09 00:19:35 +0300

    Add jobs link

--- Web/Routes.hs
+++ Web/Routes.hs
@@ -1,12 +1,12 @@
 module Web.Routes where
-import IHP.RouterPrelude
+
 import Generated.Types
+import IHP.RouterPrelude
 import Web.Types
 
 -- Generator Marker
 instance AutoRoute StaticController
 instance AutoRoute ItemsController
-
 
 instance AutoRoute BidsController
 instance AutoRoute SessionsController
--- Web/View/Layout.hs
+++ Web/View/Layout.hs
@@ -1,17 +1,20 @@
 module Web.View.Layout (defaultLayout, Html) where
 
-import IHP.ViewPrelude
-import IHP.Environment
-import qualified Text.Blaze.Html5            as H
-import qualified Text.Blaze.Html5.Attributes as A
+import Application.Helper.View
 import Generated.Types
 import IHP.Controller.RequestContext
-import Web.Types
+import IHP.Environment
+import IHP.Job.Dashboard
+import IHP.ViewPrelude
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 import Web.Routes
-import Application.Helper.View
+import Web.Types
 
 defaultLayout :: Html -> Html
-defaultLayout inner = H.docTypeHtml ! A.lang "en" $ [hsx|
+defaultLayout inner =
+    H.docTypeHtml ! A.lang "en" $
+        [hsx|
 <head>
     {metaTags}
 
@@ -30,40 +33,49 @@ defaultLayout inner = H.docTypeHtml ! A.lang "en" $ [hsx|
 |]
 
 navbar :: Html
-navbar = [hsx|
+navbar =
+    [hsx|
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="/">IHP Bid</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
+
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href={ListJobsAction}>Jobs</a>
+      </li>
+    </ul>
     {loginLogoutButton}
   </div>
 </nav>
 |]
-    where
-        loginLogoutButton :: Html
-        loginLogoutButton =
-            case fromFrozenContext @(Maybe User) of
-                Just user ->
-                    [hsx|<a class="js-delete js-delete-no-confirm text-secondary" href={DeleteSessionAction}>Logout</a>|]
-                Nothing ->
-                    [hsx|<a class="text-secondary" href={NewSessionAction}>Login</a>|]
+  where
+    loginLogoutButton :: Html
+    loginLogoutButton =
+        case fromFrozenContext @(Maybe User) of
+            Just user ->
+                [hsx|<a class="js-delete js-delete-no-confirm text-secondary" href={DeleteSessionAction}>Logout</a>|]
+            Nothing ->
+                [hsx|<a class="text-secondary" href={NewSessionAction}>Login</a>|]
 
 -- The 'assetPath' function used below appends a `?v=SOME_VERSION` to the static assets in production
 -- This is useful to avoid users having old CSS and JS files in their browser cache once a new version is deployed
 -- See https://ihp.digitallyinduced.com/Guide/assets.html for more details
 
 stylesheets :: Html
-stylesheets = [hsx|
+stylesheets =
+    [hsx|
         <link rel="stylesheet" href={assetPath "/vendor/bootstrap.min.css"}/>
         <link rel="stylesheet" href={assetPath "/vendor/flatpickr.min.css"}/>
         <link rel="stylesheet" href={assetPath "/app.css"}/>
     |]
 
 scripts :: Html
-scripts = [hsx|
+scripts =
+    [hsx|
         {when isDevelopment devScripts}
         <script src={assetPath "/vendor/jquery-3.6.0.slim.min.js"}></script>
         <script src={assetPath "/vendor/timeago.js"}></script>
@@ -79,12 +91,14 @@ scripts = [hsx|
     |]
 
 devScripts :: Html
-devScripts = [hsx|
+devScripts =
+    [hsx|
         <script id="livereload-script" src={assetPath "/livereload.js"}></script>
     |]
 
 metaTags :: Html
-metaTags = [hsx|
+metaTags =
+    [hsx|
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta property="og:title" content="App"/>
@@ -93,4 +107,3 @@ metaTags = [hsx|
     <meta property="og:description" content="TODO"/>
     {autoRefreshMeta}
 |]
-
